1fda7c9948414c59d43ee4cd21a36487fea1a36e
kwishna <kwishna@gmail.com>
2024-10-21 23:16:02 +0300

    deno own api for env

--- basics/deno-env-permission.ts
+++ basics/deno-env-permission.ts
@@ -2,4 +2,6 @@ import process from "node:process";
 
 console.log(process.env);
 // To access env, we must provide permission
-//deno run  --allow-env .\basics\DenoClass.ts
+//deno run  --allow-env .\basics\deno-env-permission.ts
+
+console.log(Deno.env)
